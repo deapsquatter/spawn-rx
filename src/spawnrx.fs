5624d53a8dbd0beedc90a1d0bcdba57c444c5c2d
kevin.knoop <kevin.knoop@centrastage.com>
2018-10-08 15:31:45 +0200

    Added Process ID.

--- src/spawnrx.fs
+++ src/spawnrx.fs
@@ -5,7 +5,8 @@ module Spawn
   open System.Diagnostics
 
   type SpawnProgress =
-    {StdOut: string option
+    {Pid: int
+     StdOut: string option
      StdErr: string option
      ExitCode: int option}
 
@@ -31,6 +32,7 @@ module Spawn
 
   let private forProcess fileName arguments environmentSettings input =
     let ofObj value = match value with null -> None | _ -> Some value
+    let getPid (p:System.Diagnostics.Process) = try p.Id with |_ -> 0
     {new IObservable<_> with
       member x.Subscribe(o) =
         let psi = match arguments with
@@ -44,10 +46,10 @@ module Spawn
         let p = Process.Start(psi)
         p.EnableRaisingEvents <- true
         p.Exited.Add
-          (fun _ -> o.OnNext({StdOut=None;StdErr=None;ExitCode=waitForExit p})
+          (fun _ -> o.OnNext({Pid=getPid p;StdOut=None;StdErr=None;ExitCode=waitForExit p})
                     o.OnCompleted())
-        p.OutputDataReceived.Add (fun t -> o.OnNext({StdOut=ofObj t.Data;StdErr=None;ExitCode=None}))
-        p.ErrorDataReceived.Add (fun t -> o.OnNext({StdOut=None;StdErr=ofObj t.Data;ExitCode=None}))
+        p.OutputDataReceived.Add (fun t -> o.OnNext({Pid=getPid p;StdOut=ofObj t.Data;StdErr=None;ExitCode=None}))
+        p.ErrorDataReceived.Add (fun t -> o.OnNext({Pid=getPid p;StdOut=None;StdErr=ofObj t.Data;ExitCode=None}))
         p.BeginOutputReadLine()
         p.BeginErrorReadLine()
         do input |> Option.iter (writeInput p)
